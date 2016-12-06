78a084f459afd685da06019524f20abe925e36f2
DEEPESH REHI <rehi.deepesh@gmail.com>
2016-12-06 12:36:50 +0530

    Create person.java

---
+++ person.java
@@ -0,0 +1,14 @@
+package com.neilson.read;
+
+public class Person {
+
+	public Person(String string, int parseInt, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
